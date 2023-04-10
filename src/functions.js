6851786fc8e5c23499f03e86a54aee09f2d50620
xanderazuaje <charon-mortis@protonmail.com>
2023-04-10 22:43:06 +0200

    created addSecondTitle function

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,18 @@
+import { FONTS } from "./constants"
+
 function addTitle(text){
     const h1 = document.createElement("h1")
-    h1.classList.add('title')
+    h1.classList.add(FONTS.title)
     h1.textContent = text
 
     return h1
 }
+function addSecondTitle(text){
+    const h2 = document.createElement("h2")
+    h2.classList.add(FONTS.bodyFont)
+    h2.textContent = text
 
-export {addTitle}
+    return h2
+}
+
+export {addTitle, addSecondTitle}
