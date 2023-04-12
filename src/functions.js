9e25551b2b328d984d01c5a7d1e97afb50781c89
xanderazuaje <charon-mortis@protonmail.com>
2023-04-12 02:37:10 +0200

    moved the SVG element inside of the loop

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,4 @@
-import { FONTS } from "./constants"
+import { FONTS, SVG_NAMESPACE } from "./constants"
 
 function addTitle(text){
     const h1 = document.createElement("h1")
@@ -30,4 +30,48 @@ function addAddressElement(title, text){
     return element
 }
 
-export {addTitle, addSecondTitle, addAddressElement}
+const starPath = "M9.54894 0.927049C9.8483 0.00573857 11.1517 0.00574037 11.4511 0.927051L13.0819 5.9463C13.2158 6.35833 13.5997 6.63729 14.033 6.63729H19.3105C20.2792 6.63729 20.682 7.8769 19.8983 8.4463L15.6287 11.5484C15.2782 11.803 15.1315 12.2544 15.2654 12.6664L16.8963 17.6857C17.1956 18.607 16.1411 19.3731 15.3574 18.8037L11.0878 15.7016C10.7373 15.447 10.2627 15.447 9.91221 15.7016L5.64258 18.8037C4.85887 19.3731 3.80439 18.607 4.10374 17.6857L5.7346 12.6664C5.86847 12.2544 5.72181 11.803 5.37132 11.5484L1.10169 8.4463C0.317977 7.8769 0.720754 6.63729 1.68948 6.63729H6.96703C7.40026 6.63729 7.78421 6.35833 7.91809 5.9463L9.54894 0.927049Z"
+const starColor = "#C56B6B"
+
+function addStars(amount){
+    const starContainer = document.createElement('div')
+        starContainer.classList.add('stars')
+
+    for (let i = 0; i < amount; i++) {
+    const starSvg = document.createElementNS(SVG_NAMESPACE, "svg")
+    starSvg.setAttribute("width", "21")
+    starSvg.setAttribute("height", "19")
+    starSvg.setAttribute("viewBox", "0 0 21 19")
+    starSvg.setAttribute("fill", "none")
+    starSvg.setAttribute("xmlns", SVG_NAMESPACE)
+
+    const starShape = document.createElementNS(SVG_NAMESPACE, "path")
+    starShape.setAttribute("d", starPath)
+    starShape.setAttribute("fill", starColor)
+    starSvg.append(starShape)
+        starContainer.append(starSvg)
+    }
+
+    return starContainer
+}
+
+function addReview(criticsName, review, stars){
+    const container = document.createElement('article')
+        container.classList.add('reviews__element')
+
+    const criticsNameElement = document.createElement('h3')
+        criticsNameElement.classList.add(FONTS.subTitle)
+        criticsNameElement.textContent = criticsName
+    container.append(criticsNameElement)
+
+    const reviewElement = document.createElement('q')
+        reviewElement.classList.add(FONTS.smallText)
+        reviewElement.textContent = review
+    container.append(reviewElement)
+
+    container.append(addStars(stars))
+
+    return container
+}
+
+export {addTitle, addSecondTitle, addAddressElement, addReview}
